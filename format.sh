92bcba5e9b565c3d40b7a08469c593bec8c40950
Yang Wang <wyatuestc@gmail.com>
2024-09-19 23:25:10 -0700

    f

--- format.sh
+++ format.sh
@@ -1,1 +1,1 @@
-yapf  --recursive . --style={based_on_s'tyle: google, column_limit: 120, indent_width: 4}' -i
+yapf  --recursive . --style='{based_on_style: google, column_limit: 120, indent_width: 4}' -i
