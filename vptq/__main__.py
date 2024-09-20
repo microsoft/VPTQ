31b39484b23de53d1e6d924a1d9e5661c3445e5a
wejoncy <wejoncy@163.com>
2024-09-20 04:45:02 -0700

    support streaming

--- vptq/__main__.py
+++ vptq/__main__.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------
 
 import argparse
+import os
 
 import transformers
 
@@ -28,21 +29,41 @@ A typical usage is:
     return parser
 
 
-def chat_loop(model, tokenizer):
+def eval_prompt(model, tokenizer, args):
+    inputs = tokenizer(args.prompt, return_tensors="pt").to(model.device)
+    streamer = transformers.TextStreamer(tokenizer)
+    model.generate(**inputs, streamer=streamer, max_new_tokens=100, pad_token_id=2)
+
+
+def chat_loop(model, tokenizer, args):
+    if not args.chat:
+        eval_prompt(model, tokenizer, args)
+        return
+
+    if getattr(tokenizer, "chat_template", None) is None:
+        print("warning: this tokenizer didn't provide chat_template.!!!")
+        eval_prompt(model, tokenizer, args)
+        return
+
     print("============================chat with the model============================")
     print("Press 'exit' to quit")
     messages = [{"role": "system", "content": "you are a math teacher."}]
+
     while True:
         text = input("You: ")
         if text == "exit":
             break
         messages.append({"role": "user", "content": text})
         encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
-        model_inputs = encodeds.to("cuda")
-        generated_ids = model.generate(model_inputs, pad_token_id=2, max_new_tokens=500, do_sample=True)
+        model_inputs = encodeds.to(model.device)
+        streamer = transformers.TextStreamer(tokenizer, skip_prompt=True, decode_kwargs={"skip_special_tokens": True})
+        generated_ids = model.generate(model_inputs,
+                                       streamer=streamer,
+                                       pad_token_id=2,
+                                       max_new_tokens=500,
+                                       do_sample=True)
         decoded = tokenizer.batch_decode(generated_ids[:, model_inputs.shape[-1]:], skip_special_tokens=True)
         messages.append({"role": "assistant", "content": decoded[0]})
-        print("assistant:", decoded[0])
 
 
 def get_valid_args(parser):
@@ -55,15 +76,15 @@ def main():
     args = get_valid_args(parser)
     print(args)
 
-    model = VQAutoModelQuantization.from_pretrained(args.model, device_map="auto").half()
-    tokenizer = transformers.AutoTokenizer.from_pretrained(args.tokenizer or args.model)
+    hf_args = {}
+    token = os.getenv("HF_TOKEN", None)
+    if token is not None:
+        hf_args["token"] = token
 
-    if args.chat:
-        chat_loop(model, tokenizer)
-        return
-    inputs = tokenizer(args.prompt, return_tensors="pt").to(model.device)
-    out = model.generate(**inputs, max_new_tokens=100, pad_token_id=2)
-    print(tokenizer.decode(out[0], skip_special_tokens=False))
+    model = VQAutoModelQuantization.from_pretrained(args.model, device_map="auto", **hf_args).half()
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.tokenizer or args.model, **hf_args)
+
+    chat_loop(model, tokenizer, args)
 
 
 main()
