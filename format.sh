5b52fb51f1efcfdaa1dfb5107cb8126143a6361f
wejoncy <247153481@qq.com>
2024-09-21 15:57:55 +0800

    publish ci (#17)

--- format.sh
+++ format.sh
@@ -1,1 +1,4 @@
 yapf  --recursive . --style='{based_on_style: google, column_limit: 120, indent_width: 4}' -i
+isort .
+find csrc/ \( -name '*.h' -o -name '*.cc' -o -name '*.cu' -o -name '*.cuh' \) -print \
+| xargs clang-format -i
