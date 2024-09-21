697e6b2e362ce33b25d3946af5a4008da2aae9c0
wejoncy <247153481@qq.com>
2024-09-21 15:57:55 +0800

    publish ci (#17)

--- vptq/layers/__init__.py
+++ vptq/layers/__init__.py
@@ -3,5 +3,4 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.1"
-from .layers import AutoModelForCausalLM as AutoModelForCausalLM
+from .model_base import AutoModelForCausalLM as AutoModelForCausalLM
