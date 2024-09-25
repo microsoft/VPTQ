535dcb16b320323e7330fe551a6a90df51a9160c
wejoncy <247153481@qq.com>
2024-09-25 18:44:59 +0800

    fix out of bound error (#20)

--- csrc/common.h
+++ csrc/common.h
@@ -5,6 +5,32 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/extension.h>
 
+class OptionalCUDAGuard {
+  int set_device_ = -1;
+  int current_device_ = -1;
+
+ public:
+  OptionalCUDAGuard(int device) : set_device_(device) {
+    cudaError_t err = cudaGetDevice(&current_device_);
+    std::stringstream ss;
+    if (err != cudaSuccess) {
+      ss << "cudaGetDevice failed with error code " << cudaGetErrorString(err);
+      TORCH_CHECK(err == cudaSuccess, ss.str());
+    }
+    if (current_device_ == device) {
+      return;
+    }
+    err = cudaSetDevice(device);
+    if (err != cudaSuccess) {
+      ss << "cudaGetDevice failed with error code " << cudaGetErrorString(err);
+      TORCH_CHECK(err == cudaSuccess, ss.str());
+    }
+  }
+  ~OptionalCUDAGuard() {
+    if (set_device_ != current_device_) cudaSetDevice(current_device_);
+  }
+};
+
 #define gpuErrchk(ret) gpuAssert((ret), __FILE__, __LINE__);
 
 inline void gpuAssert(cudaError_t code, const char* file, int line) {
