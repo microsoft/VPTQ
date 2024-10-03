f0b42ce8744206fbab1c2410e9ac38c0feae9727
Yang Wang <wyatuestc@gmail.com>
2024-10-03 19:28:24 +0800

    add prompt args and check cuda kernel (#36)
    add prompt args and check cuda kernel
    
    ---------
    
    Co-authored-by: OpenSourceRonin <OpenSourceRonin@OpenSourceRonin.tech>
    Co-authored-by: OpenSourceRonin <OpenSourceRonin>

--- vptq/layers/model_base.py
+++ vptq/layers/model_base.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------
 
 import glob
+import importlib.util
 from pathlib import Path
 
 import accelerate
@@ -106,6 +107,13 @@ class AutoModelForCausalLM(transformers.AutoModelForCausalLM):
             dtype=torch_dtype,
             # preload_module_classes=["VQuantLinear"]
         )
+
+        # check cuda kernel exist
+        if importlib.util.find_spec("vptq.ops") is not None:
+            pass
+        else:
+            print('!!! Warning !!!: CUDA kernel not found, please check CUDA and VPTQ installation.')
+            print('!!! Warning !!!: Running on Torch Implementation, which is extremely slow.')
 
         # weight_bins = glob.glob(str(Path(pretrained_model_name_or_path).absolute() / '*.safetensors'))
         # all_missing_keys = []
