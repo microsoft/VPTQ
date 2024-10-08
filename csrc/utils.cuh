2d6d252cf19a075131ebc2e73d557e1fab5e0303
wejoncy <247153481@qq.com>
2024-10-08 14:47:56 +0800

    support bf16 (#51)
    Gracefully support bf16 for all nvidia-GPUs.
    For thoce old GPU like V100/T4, we just do the simulation and the
    computation actually happens in float32.
    This would be slower than half then.

--- csrc/utils.cuh
+++ csrc/utils.cuh
@@ -20,10 +20,18 @@ struct TypeVec2<__nv_bfloat16> {
   typedef __nv_bfloat162 type;
 };
 
+template <>
+struct TypeVec2<float> {
+  typedef float2 type;
+};
+
 template <typename T>
 T __device__ __forceinline__ ConvertFromFloat(float v, T vv) {
+  (void)(vv);
   if constexpr (std::is_same<T, __nv_bfloat16>::value) {
     return vv = __float2bfloat16(v);
+  } else if constexpr (std::is_same<T, float>::value) {
+    return vv = v;
   } else {
     static_assert(std::is_same<T, __half>::value);
     return vv = __float2half(v);
@@ -34,6 +42,8 @@ template <typename T>
 float __device__ __forceinline__ ConvertToFloat(T v) {
   if constexpr (std::is_same<T, __nv_bfloat16>::value) {
     return __bfloat162float(v);
+  } else if constexpr (std::is_same<T, float>::value) {
+    return v;
   } else {
     static_assert(std::is_same<T, __half>::value);
     return __half2float(v);
@@ -50,8 +60,12 @@ __device__ __forceinline__ float warpReduceSum(float sum) {
   return sum;
 }
 
-template <int GROUPSIZE>
-__device__ __forceinline__ void ldg_vec_x(uint32_t* __restrict__ dst_u32, const uint32_t* __restrict__ src_u32) {
+template <int GROUPSIZE, typename T>
+__device__ __forceinline__ void ldg_vec_x(T* __restrict__ dst_t32, const uint32_t* __restrict__ src_u32) {
+  uint32_t* dst_u32 = (uint32_t*)dst_t32;
+  if constexpr (std::is_same<T, float>::value || std::is_same<T, float2>::value) {
+    return ldg_vec_x<GROUPSIZE * 2>(dst_u32, src_u32);
+  }
   int2* dst = (int2*)dst_u32;
   const int2* src = (const int2*)src_u32;
   if constexpr (GROUPSIZE == 2) {
@@ -88,9 +102,9 @@ __device__ __forceinline__ void ldg_vec_x(uint32_t* __restrict__ dst_u32, const 
   } else if constexpr (GROUPSIZE == 12) {
     if (uint64_t(src) % 16) {
       dst[0] = __ldg(src);
-      int4 b = __ldg((int4*)(src + 1));
-      dst[1] = *((int2*)&b);
-      dst[2] = *((int2*)&b + 1);
+      int4 b = __ldg((const int4*)(src + 1));
+      dst[1] = *((const int2*)&b);
+      dst[2] = *((const int2*)&b + 1);
     } else {
       *(int4*)dst = __ldg((int4*)(src));
       dst[2] = __ldg((src + 2));
@@ -110,6 +124,25 @@ __device__ __forceinline__ void ldg_vec_x(uint32_t* __restrict__ dst_u32, const 
     //       : "=r"(dec[4]), "=r"(dec[5])
     //       : "l"((const void*)src)
     //     );
+  } else if constexpr (GROUPSIZE == 24) {
+    *((int4*)(dst)) = __ldg((const int4*)(src));
+    *(((int4*)(dst)) + 1) = __ldg(((const int4*)(src)) + 1);
+    *(((int4*)(dst)) + 2) = __ldg(((const int4*)(src)) + 2);
+  } else if constexpr (GROUPSIZE == 32) {
+    asm volatile("ld.cg.global.v4.u32 {%0, %1, %2, %3}, [%4];"
+                 : "=r"(dst_u32[0]), "=r"(dst_u32[1]), "=r"(dst_u32[2]), "=r"(dst_u32[3])
+                 : "l"((const void*)src_u32));
+    asm volatile("ld.cg.global.v4.u32 {%0, %1, %2, %3}, [%4];"
+                 : "=r"(dst_u32[4]), "=r"(dst_u32[5]), "=r"(dst_u32[6]), "=r"(dst_u32[7])
+                 : "l"((const void*)(src_u32 + 4)));
+    asm volatile("ld.cg.global.v4.u32 {%0, %1, %2, %3}, [%4];"
+                 : "=r"(dst_u32[8]), "=r"(dst_u32[9]), "=r"(dst_u32[10]), "=r"(dst_u32[11])
+                 : "l"((const void*)(src_u32 + 8)));
+    asm volatile("ld.cg.global.v4.u32 {%0, %1, %2, %3}, [%4];"
+                 : "=r"(dst_u32[12]), "=r"(dst_u32[13]), "=r"(dst_u32[14]), "=r"(dst_u32[15])
+                 : "l"((const void*)(src_u32 + 12)));
+  } else {
+    assert(false);
   }
 }
 
@@ -144,3 +177,39 @@ __forceinline__ T ceil_div(T a, T b) {
 }
 
 }  // namespace cuda
+
+template <typename T>
+T __device__ __forceinline__ FMA2(T a, T b, T c) {
+  if constexpr (std::is_same<T, __nv_bfloat162>::value) {
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 800
+    float x = __bfloat162float(a.x) * __bfloat162float(b.x) + __bfloat162float(c.x);
+    float y = __bfloat162float(a.y) * __bfloat162float(b.y) + __bfloat162float(c.y);
+    return __nv_bfloat162{__float2bfloat16(x), __float2bfloat16(y)};
+#else
+    return __hfma2(a, b, c);
+#endif
+  } else if constexpr (std::is_same<T, float2>::value) {
+    return float2{a.x * b.x + c.x, a.y * b.y + c.y};
+  } else {
+    return __hfma2(a, b, c);
+  }
+  __builtin_unreachable();  // Suppress missing return statement warning
+}
+
+template <typename T>
+T __device__ __forceinline__ ADD2(T a, T b) {
+  if constexpr (std::is_same<T, __nv_bfloat162>::value) {
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 800
+    float x = __bfloat162float(a.x) + __bfloat162float(b.x);
+    float y = __bfloat162float(a.y) + __bfloat162float(b.y);
+    return __nv_bfloat162{__float2bfloat16(x), __float2bfloat16(y)};
+#else
+    return __hadd2(a, b);
+#endif
+  } else if constexpr (std::is_same<T, float2>::value) {
+    return float2{a.x + b.x, a.y + b.y};
+  } else {
+    return __hadd2(a, b);
+  }
+  __builtin_unreachable();  // Suppress missing return statement warning
+}
