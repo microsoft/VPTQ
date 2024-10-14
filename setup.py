b111810eaccd35b5ae3e84b605144b2ca8c24682
Yang Wang <wyatuestc@gmail.com>
2024-10-14 01:41:34 -0700

    update setup to skip compile

--- setup.py
+++ setup.py
@@ -22,6 +22,10 @@ def get_version():
 
 
 def build_cuda_extensions():
+    if os.getenv("SKIP_COMPILE", "0") == "1":
+        print("Skipping CUDA/ROCm extension compilation due to SKIP_COMPILE setting")
+        return []
+    
     compute_capabilities = [70, 75, 80, 86, 89, 90]
     arch_flags = []
     TORCH_CUDA_ARCH_LIST = os.getenv("TORCH_CUDA_ARCH_LIST", None)
