ecff26f0968d907f476dac55aa886df53fd189d8
Yang Wang <wyatuestc@gmail.com>
2024-10-17 02:04:26 -0700

    fix format

--- vptq/app.py
+++ vptq/app.py
@@ -61,8 +61,7 @@ models = [
 ]
 
 model_choices = [f"{model['name']} ({model['bits']})" for model in models]
-display_to_model = {
-    f"{model['name']} ({model['bits']})": model['name'] for model in models}
+display_to_model = {f"{model['name']} ({model['bits']})": model['name'] for model in models}
 
 
 def download_model(model):
@@ -144,12 +143,9 @@ with gr.Blocks(fill_height=True) as demo:
         chat_interface = gr.ChatInterface(
             respond,
             additional_inputs=[
-                gr.Textbox(value="You are a friendly Chatbot.",
-                           label="System message"),
-                gr.Slider(minimum=1, maximum=2048, value=512,
-                          step=1, label="Max new tokens"),
-                gr.Slider(minimum=0.1, maximum=4.0, value=0.7,
-                          step=0.1, label="Temperature"),
+                gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
+                gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
+                gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
                 gr.Slider(
                     minimum=0.1,
                     maximum=1.0,
@@ -169,8 +165,7 @@ if __name__ == "__main__":
     share = os.getenv("SHARE_LINK", None) in ["1", "true", "True"]
     download = os.getenv("DOWNLOAD_MODEL", None) in ["1", "true", "True"]
     if download:
-        download_thread = threading.Thread(
-            target=download_models_in_background)
+        download_thread = threading.Thread(target=download_models_in_background)
         download_thread.start()
     demo.launch(share=share)
     disable_gpu_info()
