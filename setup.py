8426d1795559a5daa2766ce96bbccdcf64052eaf
lcy.seso <lcy.seso@gmail.com>
2024-12-27 02:37:31 +0000

    build using cmake.

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@
 # --------------------------------------------------------------------------
 
 import os
+import subprocess
 from pathlib import Path
 
-import torch
-from setuptools import find_packages, setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from setuptools import Command, Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext
+
+# from setuptools.command.install_lib import install_lib
 
 cur_path = Path(__file__).parent
 
@@ -21,77 +23,6 @@ def get_version():
     return "0.0.1"
 
 
-def build_cuda_extensions():
-    compute_capabilities = [70, 75, 80, 86, 89, 90]
-    arch_flags = []
-    TORCH_CUDA_ARCH_LIST = os.getenv("TORCH_CUDA_ARCH_LIST", None)
-    if TORCH_CUDA_ARCH_LIST is None:
-        print("TORCH_CUDA_ARCH_LIST is not set, compiling for all arch")
-    else:
-        delimiter = ' ' if ';' not in TORCH_CUDA_ARCH_LIST else ' '
-        TORCH_CUDA_ARCH_LIST = TORCH_CUDA_ARCH_LIST.split(delimiter)
-        compute_capabilities = [
-            int(10 * float(arch))
-            for arch in TORCH_CUDA_ARCH_LIST
-            if '+' not in arch
-        ]
-
-    if torch.cuda.is_available() and torch.version.hip is not None:
-        PYTORCH_ROCM_ARCH = os.getenv("PYTORCH_ROCM_ARCH", None)
-        arch_name = torch.cuda.get_device_properties().gcnArchName.split(":")[0]
-        if PYTORCH_ROCM_ARCH is not None and arch_name not in PYTORCH_ROCM_ARCH:
-            PYTORCH_ROCM_ARCH = PYTORCH_ROCM_ARCH + f";{arch_name}"
-        elif PYTORCH_ROCM_ARCH is None:
-            PYTORCH_ROCM_ARCH = arch_name
-        compute_capabilities = PYTORCH_ROCM_ARCH
-        os.environ["PYTORCH_ROCM_ARCH"] = PYTORCH_ROCM_ARCH
-    else:
-        for cap in compute_capabilities:
-            arch_flags += ["-gencode", f"arch=compute_{cap},code=sm_{cap}"]
-    print((" build for compute capabilities: "
-           "=============="), compute_capabilities)
-
-    # set nvcc threads
-    nvcc_threads = os.getenv("NVCC_THREADS") or "4"
-
-    extra_compile_args = {
-        "nvcc": [
-            "-O3",
-            "-std=c++17",
-            "-DENABLE_BF16",
-            "-U__CUDA_NO_HALF_OPERATORS__",
-            "-U__CUDA_NO_HALF_CONVERSIONS__",
-            "-U__CUDA_NO_HALF2_OPERATORS__",
-            "-U__CUDA_NO_BFLOAT16_OPERATORS__",
-            "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
-            "-U__CUDA_NO_BFLOAT162_OPERATORS__",
-            "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
-            f"--threads={nvcc_threads}",
-        ] + arch_flags,
-        "cxx": ["-O3", "-fopenmp", "-lgomp", "-std=c++17", "-DENABLE_BF16"],
-    }
-
-    if torch.cuda.is_available() and torch.version.hip is not None:
-        extra_compile_args["nvcc"].extend(["-fbracket-depth=1024"])
-    else:
-        extra_compile_args["nvcc"].extend([
-            "--expt-relaxed-constexpr",
-            "--expt-extended-lambda",
-            "--use_fast_math",
-            "-lineinfo",
-        ])
-
-    extensions = CUDAExtension(
-        "vptq.cuda_ops",
-        [
-            "csrc/ops.cc",
-            "csrc/dequant_impl_packed.cu",
-        ],
-        extra_compile_args=extra_compile_args,
-    )
-    return [extensions]
-
-
 def get_requirements():
     """Get Python package dependencies from requirements.txt."""
     with open(cur_path / "requirements.txt") as f:
@@ -100,12 +31,149 @@ def get_requirements():
     return requirements
 
 
+class CMakeExtension(Extension):
+    """ specify the root folder of the CMake projects"""
+
+    def __init__(self, name="cuda_ops", cmake_lists_dir=".", **kwargs):
+        Extension.__init__(self, name, sources=[], **kwargs)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
+
+
+class CMakeBuildExt(build_ext):
+    """launches the CMake build."""
+
+    def copy_extensions_to_source(self) -> None:
+        build_py = self.get_finalized_command("build_py")
+        for ext in self.extensions:
+            source_path = os.path.join(self.build_lib, "lib" + ext.name + ".so")
+            inplace_file, _ = self._get_inplace_equivalent(build_py, ext)
+
+            target_path = os.path.join(build_py.build_lib, "vptq", inplace_file)
+
+            # Always copy, even if source is older than destination, to ensure
+            # that the right extensions for the current Python/platform are
+            # used.
+            if os.path.exists(source_path) or not ext.optional:
+                self.copy_file(source_path, target_path, level=self.verbose)
+
+    def build_extension(self, ext: CMakeExtension) -> None:
+        # Ensure that CMake is present and working
+        try:
+            subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError("Cannot find CMake executable") from None
+
+        debug = int(
+            os.environ.get("DEBUG", 0)
+        ) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
+
+        parallel_level = os.environ.get("CMAKE_BUILD_PARALLEL_LEVEL", None)
+        if parallel_level is not None:
+            self.parallel = int(parallel_level)
+        else:
+            self.parallel = os.cpu_count()
+
+        for ext in self.extensions:
+            extdir = os.path.abspath(
+                os.path.dirname(self.get_ext_fullpath(ext.name))
+            )
+
+            cmake_args = [
+                "-DCMAKE_BUILD_TYPE=%s" % cfg,
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(
+                    cfg.upper(), extdir
+                ), "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}".format(
+                    cfg.upper(), self.build_temp
+                )
+            ]
+
+            # Adding CMake arguments set as environment variable
+            if "CMAKE_ARGS" in os.environ:
+                cmake_args += [
+                    item for item in os.environ["CMAKE_ARGS"].split(" ") if item
+                ]
+
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+
+            build_args = []
+            build_args += ["--config", cfg]
+            # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+            # across all generators.
+            if (
+                "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ and
+                hasattr(self, "parallel") and self.parallel
+            ):
+                build_args += [f"-j{self.parallel}"]
+
+            build_temp = Path(self.build_temp) / ext.name
+            if not build_temp.exists():
+                build_temp.mkdir(parents=True)
+
+            # Config
+            subprocess.check_call(["cmake", ext.cmake_lists_dir] + cmake_args,
+                                  cwd=self.build_temp)
+
+            # Build
+            subprocess.check_call(["cmake", "--build", "."] + build_args,
+                                  cwd=self.build_temp)
+            print()
+            self.copy_extensions_to_source()
+
+
+class Clean(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import glob
+        import re
+        import shutil
+
+        with open(".gitignore") as f:
+            ignores = f.read()
+            pat = re.compile(r"^#( BEGIN NOT-CLEAN-FILES )?")
+            for wildcard in filter(None, ignores.split("\n")):
+                match = pat.match(wildcard)
+                if match:
+                    if match.group(1):
+                        # Marker is found and stop reading .gitignore.
+                        break
+                    # Ignore lines which begin with '#'.
+                else:
+                    # Don't remove absolute paths from the system
+                    wildcard = wildcard.lstrip("./")
+
+                    for filename in glob.glob(wildcard):
+                        print(f"cleaning '{filename}'")
+                        try:
+                            os.remove(filename)
+                        except OSError:
+                            shutil.rmtree(filename, ignore_errors=True)
+
+
+description = (
+    "VPTQ: Extreme Low-bit Vector Post-Training Quantization "
+    "for Large Language Models"
+)
+
 setup(
     name="vptq",
     python_requires=">=3.8",
     packages=find_packages(exclude=[""]),
     install_requires=get_requirements(),
     version=get_version(),
-    ext_modules=build_cuda_extensions(),
-    cmdclass={"build_ext": BuildExtension},
+    description=description,
+    author="Wang Yang, Wen JiCheng",
+    ext_modules=[CMakeExtension()],
+    cmdclass={
+        "build_ext": CMakeBuildExt,
+        "clean": Clean,
+    },
 )
