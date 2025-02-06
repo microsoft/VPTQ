7b0cbd1b9fb2155b54bbe999d0db3b7ee11f9e49
lcy.seso <lcy.seso@gmail.com>
2025-02-06 00:57:04 +0000

    Added cutlass into dependence and pinned version to v3.7.0.

--- run_build.sh
+++ run_build.sh
@@ -13,40 +13,40 @@ unset TORCH_CUDA_ARCH_LIST
 export NVCC_THREADS=`nproc`
 
 # python3 setup.py clean
-# python3 setup.py develop 2>&1 | tee build.log
+python3 setup.py develop 2>&1 | tee build.log
 
 # TORCH_VERSION=`python -c "import torch; print(torch.__version__)"`
 # python3 setup.py build bdist_wheel \
 #   --dist-dir="../torch$TORCH_VERSION-dist" 2>&1 | tee build.log
 
-SOURCE=build/lib.linux-x86_64-cpython-312/vptq/libvptq.so
-
-cd build/temp.linux-x86_64-cpython-312
-
+# SOURCE=build/lib.linux-x86_64-cpython-312/vptq/libvptq.so
+# 
+# cd build/temp.linux-x86_64-cpython-312
+# 
 # if [ -d "CMakeFiles" ]; then
 #   rm -rf CMakeFiles
 # fi
-
+# 
 # if [ -f "CMakeCache.txt" ]; then
 #   rm -f CMakeCache.txt
 # fi
-
+# 
 # cmake ../../
-
-if [ -f "$SOURCE" ]; then
-   rm "$SOURCE"
-fi
-
-make -j32 2>&1 | tee ../../build.log
-
-cd ../../
-
-TARGET=vptq/libvptq.so
-
-if [ -f "$TARGET" ]; then
-   rm "$TARGET"
-fi
-
-cp $SOURCE $TARGET
-
-./test.sh
+# 
+# if [ -f "$SOURCE" ]; then
+#    rm "$SOURCE"
+# fi
+# 
+# make -j32 2>&1 | tee ../../build.log
+# 
+# cd ../../
+# 
+# TARGET=vptq/libvptq.so
+# 
+# if [ -f "$TARGET" ]; then
+#    rm "$TARGET"
+# fi
+# 
+# cp $SOURCE $TARGET
+# 
+# ./test.sh
